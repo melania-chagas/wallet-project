6845b5f19537e113b78d2be8bc0cbfe3b7cf7c7d
Melania P Chagas <melaniapchagas@gmail.com>
2022-07-28 18:05:09 -0300

    Configurações do Redux

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,20 @@
+import { USER_EMAIL } from '../actions/actionsTypes';
+
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE = {
+  email: '',
+
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case USER_EMAIL:
+    return { ...state, email: action.state };
+
+  default:
+    return state;
+  }
+}
+
+export default user;
