2da46bea149bd69313f037818a955a722438d157
Melania P Chagas <melaniapchagas@gmail.com>
2022-08-02 19:12:31 -0300

    Requisito 3: completo

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,30 +1,53 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import WalletForm from '../components/WalletForm';
+import './wallet.css';
+import { getCurrencies } from '../redux/actions';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    this.getAllCurrencies();
+  }
+
+  getAllCurrencies = async () => {
+    const { dispatchCurrencies } = this.props;
+    const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const data = await response.json();
+    const arrayData = Object.keys(data);
+    const filteredDataList = arrayData.filter((currencie) => currencie !== 'USDT');
+    dispatchCurrencies(filteredDataList);
+  }
+
   render() {
     const { email } = this.props;
     return (
       <div className="wallet">
         <header className="header">
-          <h1>
+          <h2>
             Wallet
-          </h1>
+          </h2>
           <h2 data-testid="email-field">{email}</h2>
           <h2 data-testid="total-field">{ `Despesa total:R$ ${0}`}</h2>
-          <p data-testid="header-currency-field">BRL</p>
+          <h3 data-testid="header-currency-field">BRL</h3>
         </header>
+        <WalletForm />
       </div>
     );
   }
 }
 
 Wallet.propTypes = {
+  dispatchCurrencies: PropTypes.func.isRequired,
   email: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
 });
-export default connect(mapStateToProps)(Wallet);
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchCurrencies: (currencies) => dispatch(getCurrencies(currencies)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,8 +1,14 @@
+import { CURRENCIES } from '../actions';
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  currencies: [],
+};
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case CURRENCIES:
+    return { ...state, currencies: action.currencies };
+
   default:
     return state;
   }
